ee58446bd089ef8f26c66d20521ab170b59f2e7b
PrashanthVj <106900374+Prasanth-Ravichandar@users.noreply.github.com>
2024-06-29 12:51:33 +0530

    Add files via upload

---
+++ src/assets/services_data.js
@@ -0,0 +1,34 @@
+const Services_Data = [
+    {
+        s_no:"01",
+        s_name:"Web design",
+        s_desc:"Create Masterfull websites based upon Requirements"
+    },
+    {
+        s_no:"02",
+        s_name:"Graphics design",
+        s_desc:"Adobe illustrator , Photoshop, Figma Designs(UI/UX)"
+    },
+    {
+        s_no:"03",
+        s_name:"Social media",
+        s_desc:"Social Media Management,Paid Social Media advertisement and Tracking Analytics and Reporting"
+    },
+    {
+        s_no:"04",
+        s_name:"SEO",
+        s_desc:"Find Related Keyphrases to enhance the website ranking."
+    },
+    {
+        s_no:"05",
+        s_name:"Digital marketing",
+        s_desc:"Social Media Marketing & Email Marketing"
+    },
+    {
+        s_no:"06",
+        s_name:"Content writing",
+        s_desc:"Writing blogs contents for promoting websites content."
+    },
+]
+ 
+export default Services_Data;
